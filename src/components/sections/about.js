bf9919d3deaaf212a44cbfc1113799cb9a0eccc0
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-06-06 21:00:36 +0500

    feature: rebranding and adding content

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -81,8 +81,8 @@ const StyledPic = styled.div`
     .img {
       position: relative;
       border-radius: var(--border-radius);
-      mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1);
+      // mix-blend-mode: multiply;
+      filter: grayscale(50%) contrast(1);
       transition: var(--transition);
     }
 
@@ -125,7 +125,15 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React',
+    'Angular',
+    'Node.js',
+    'Express',
+    'MongoDB',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +143,25 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              I'm a Senior Software Engineer with 4 years of expertise in designing and developing
+              web applications using JavaScript, React, Angular, Express and Node. I possess
+              excellent communication and presentation skills and am well-versed in requirement
+              gathering, and estimation. My energy and experience are always dedicated to building
+              efficient and impactful web applications while ensuring cross-browser compatibility
+              and performance optimization.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Today, I’ve had the privilege of working for{' '}
+              <a href="https://shapepowered.com/">SHAPE</a> ( featured article in Forbes360 ),{' '}
+              <a href="/">a start-up</a>, and{' '}
+              <a href="https://www.aerodyne.group/">a huge corporation</a>, My main focus these days
+              is building digital experiences at <a href="https://www.aerodyne.group/">Aerodyne</a>{' '}
+              for a variety of clients.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I'm also exploring several ideas to integrate with LLMs using MERN stack approach.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
